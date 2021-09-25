73da558af81f64212ed16f544e6a167f169aa2aa
Pansilu Gunaratna <pansilugunaratne8@gmail.com>
2021-09-25 21:52:50 +0530

    add social media tab

---
+++ js/social_media.js
@@ -0,0 +1,10 @@
+const tab = document.getElementById("icon_tab");
+
+function hideShow(){
+    if(tab.style.display === "flex"){
+        tab.style.display = "none";
+    }
+    else{
+        tab.style.display === "flex";
+    }
+}
